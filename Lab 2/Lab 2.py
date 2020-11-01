32d50f29788e46989e3b6eb785048f4d6742c4f3
andri735 <aa9921020@gmail.com>
2020-11-01 21:01:31 +0700

    Lab 1

---
+++ Lab 2/Lab 2.py
@@ -0,0 +1,13 @@
+#konversi nilai variable
+
+a=input("masukkan nilai a:")
+b=input("masukkan nilai b:")
+print("variable a=",a)
+print("variable b=",b)
+
+a=int(a)
+b=int(b)
+
+print("hasil penggabungan {0}&{1}=%d".format(a,b) %(a+b))
+print("hasil penjumlahan {0}+{1}=%d".format(a,b) %(a+b))
+print("hasil pembagian {0}/{1}=%d".format(a,b) %(a/b))
---
+++ Praktikum/Tugas pertemuan 5.py
@@ -0,0 +1,15 @@
+#Mengambil input
+namalengkap=input("Siapa nama lengkap kamu: ")
+namapanggilan=input("Apa nama panggilan kamu: ")
+NPM=input("Berapa no npm kamu: ")
+tempatlahir=input("Dimana tempat lahir kamu: ")
+umur=input("Berapa umur kamu: ")
+tanggallahir=input("Berapa tanggal lahir kamu: ")
+alamat=input("Dimana alamat kamu : ")
+telepon=input("Berapa no telepon kamu: ")
+
+#Menampilkan output
+print("---------------------------------------------")
+print("Assalamu'alaikum")
+print("Let me introduce my self. My name is", namalengkap,"but you can call me", namapanggilan,"." "My NPM is", NPM,".", "I was born in", tempatlahir,"and I am", umur,"years old." "I am very glad if you want to invite my house in", alamat,"." "So don't forget to call me before with the number",telepon,".")
+print("Thank you.")
